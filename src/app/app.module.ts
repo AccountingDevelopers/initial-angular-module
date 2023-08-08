841a6a7a4e2f4a31b2d34a8aa6fc6c7c092844fc
unknown <millerkirillmail@gmail.com>
2023-08-08 20:57:56 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgAccountingModule } from 'ng-accounting';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgAccountingModule.forRoot({
+      api: {
+        key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGFlZGY2YmRmMWRhOTkwZWI0MmI1YjQiLCJjb21wYW55SWQiOiI2NGFmZmQ4ZDhlYjFkMzJhODBlM2I0YWIiLCJ3b3Jrc3BhY2VOYW1lIjoiam91cm5leSIsImlhdCI6MTY5MDU2MjAxMSwiZXhwIjoxNzE2NDgyMDExfQ.BRZ-3OIJ4qMpoH0CbEFr05mHfQKV4r1nZlKOuWiXv8U',
+        url: {
+          client: 'http://localhost:4200',
+          server: isDevMode() ? 'http://localhost:80/api/v1' : 'http://localhost:80/api/v1'
+        }
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
